1b4fe1159b65f804b43c5712a3713ee45b199a63
sunnykhatter <lakshaykhatter@gmail.com>
2013-07-08 17:41:09 -0400

    Displaying price in dollars

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -80,4 +80,11 @@ class ProductsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+ def formatted_price
+    price_in_dollars = price_in_cents.to_f / 100
+    sprintf("%.2f", price_in_dollars)
+  end
+
+
 end
