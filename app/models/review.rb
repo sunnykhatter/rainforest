b179e048358eb645d6e49e6909dbcf127d2f8fce
sunnykhatter <lakshaykhatter@gmail.com>
2013-07-09 16:23:19 -0400

    Created a functioning website

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ActiveRecord::Base
-  attr_accessible :comment, :product_id, :user_id
- 
+  attr_accessible :comment, :product_id, 
+  
   belongs_to :user
   belongs_to :product
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :password, :on => :create
 
-
+has_many :reviews
+has_many :products, :through => :reviews
 
 
 
