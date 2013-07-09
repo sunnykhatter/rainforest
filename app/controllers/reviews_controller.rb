2492f4428186b3e328038d1a3b0658961dd8bcec
sunnykhatter <lakshaykhatter@gmail.com>
2013-07-09 15:39:20 -0400

    Adding flash alerts and notices

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,29 @@
+class ReviewsController < ApplicationController
+  before_filter :load_product
+
+  def show
+  	@review = Review.find(params[:id])
+  end
+
+  def create
+
+  	@review = @product.reviews.build(params[:review])
+
+  	if @review.save
+  		redirect_to products_path, notice: 'Review created successfully'
+  	else 
+  		render :action => :show
+  	end
+  end
+
+  def destroy
+  	@review = Review.find(params[:id])
+  	@review.destroy
+  end
+
+  private
+
+  def load_product
+  	@product = Product.find(params[:product_id])
+  end
+end
---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ActiveRecord::Base
+  attr_accessible :comment, :product_id, :user_id
+ 
+  belongs_to :user
+  belongs_to :product
+
+end
