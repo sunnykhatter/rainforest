5eff064ecd2e8aa1331dd996085e93406807b4d2
sunnykhatter <lakshaykhatter@gmail.com>
2013-07-10 14:29:48 -0400

    Adding user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :name
   has_secure_password
   validates_presence_of :password, :on => :create
 
 has_many :reviews
 has_many :products, :through => :reviews
-
+	
+	validates_presence_of :name
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130709180257) do
+ActiveRecord::Schema.define(:version => 20130710181544) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20130709180257) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "name"
   end
 
 end
